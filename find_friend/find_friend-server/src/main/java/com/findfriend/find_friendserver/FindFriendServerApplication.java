73a55a2b956e7536bf7a7e054dba1ab04b547eba
lrz168 <2506979476@qq.com>
2024-02-15 22:54:09 +0800

    init structure

---
+++ find_friend/find_friend-server/src/main/java/com/findfriend/find_friendserver/FindFriendServerApplication.java
@@ -0,0 +1,13 @@
+package com.findfriend.find_friendserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindFriendServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindFriendServerApplication.class, args);
+    }
+
+}
