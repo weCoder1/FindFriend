c35385e170598777b23e8834edd75367880398c6
RuizeLi8 <ruize.lee8@gmail.com>
2024-03-03 14:03:34 +0800

    解决依赖冲突

--- find_friend/find_friend-pojo/src/main/java/com/findFriend/entity/User.java
+++ find_friend/find_friend-pojo/src/main/java/com/findFriend/entity/User.java
@@ -23,16 +23,41 @@ public class User implements Serializable {
     @TableId(type = IdType.AUTO)
     private Long id;
 
+    @TableField("name")
     private String name;
+
+    @TableField("username")
     private String username;
+
+    @TableField("password")
     private String password;
+
+    @TableField("phone")
     private String phone;
+
+    //1男，0女
+    @TableField("sex")
     private String sex;
+
+    @TableField("image")
     private String image;
+
+    @TableField("type")
     private Integer type;
+
+    @TableField("status")
     private Integer status;
+
+    @TableField("create_time")
     private LocalDateTime createTime;
+
+    @TableField("update_time")
     private LocalDateTime updateTime;
+
+
+    @TableField("create_user")
     private Long createUser;
+
+    @TableField("update_user")
     private Long updateUser;
 }
--- find_friend/find_friend-server/src/main/java/com/findfriend/service/UserService.java
+++ find_friend/find_friend-server/src/main/java/com/findfriend/service/UserService.java
@@ -3,7 +3,6 @@ package com.findFriend.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.findFriend.dto.UserDTO;
 import com.findFriend.entity.User;
-import com.findFriend.mapper.UserMapper;
 
 public interface UserService extends IService<User> {
 
