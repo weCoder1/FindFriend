cd52138d076ec2217677fe076b71638e09a4da49
RuizeLi8 <ruize.lee8@gmail.com>
2024-03-05 15:01:13 +0800

    添加swagger接口文档

---
+++ find_friend/find_friend-server/src/main/java/com/findfriend/service/ICategoryService.java
@@ -0,0 +1,16 @@
+package com.findFriend.service;
+
+import com.findFriend.entity.Category;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 贴吧分类表 服务类
+ * </p>
+ *
+ * @author lrz
+ * @since 2024-03-03
+ */
+public interface ICategoryService extends IService<Category> {
+
+}
